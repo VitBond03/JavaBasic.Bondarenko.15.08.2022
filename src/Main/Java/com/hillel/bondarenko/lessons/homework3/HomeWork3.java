7aa1e06f5564bba5de06bed6ba86202adcec4a88
Bondarenko Vitaliy <vitbond2407@gmail.com>
2022-08-24 23:02:17 +0300

    Term

---
+++ src/Main/Java/com/hillel/bondarenko/lessons/homework3/HomeWork3.java
@@ -0,0 +1,19 @@
+package com.hillel.bondarenko.lessons.homework3;
+
+public class HomeWork3 {
+    public static void main(String[] args) {
+        int a=3;
+         int b=4;
+         int h=2;
+        System.out.println (a * b * h);
+        System.out.println ("24=volume");
+         System.out.println(24*6);
+          System.out.println("144=length");
+
+
+
+
+    }
+
+
+}
