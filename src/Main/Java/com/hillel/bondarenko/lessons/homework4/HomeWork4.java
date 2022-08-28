f127ca271f1afb45b51da5624787e52bd7304081
Bondarenko Vitaliy <vitbond2407@gmail.com>
2022-08-28 18:52:34 +0300

    Term2

--- src/Main/Java/com/hillel/bondarenko/lessons/homework4/HomeWork4.java
+++ src/Main/Java/com/hillel/bondarenko/lessons/homework4/HomeWork4.java
@@ -1,4 +1,28 @@
 package com.hillel.bondarenko.lessons.homework4;
 
 public class HomeWork4 {
+    public static void main(String[] args) {
+        int a=13;
+        int b=24;
+        int c=46;
+        int allways=860;
+        System.out.println(a*allways);
+        System.out.println(b*allways);
+        System.out.println(c*allways);
+        int sum=11180;
+        int sum2=20640;
+        int sum3=39560;
+        System.out.println(sum*1.5);
+        System.out.println(sum2*1.5);
+        System.out.println(sum3*1.5);
+
+
+
+
+
+    }
 }
+
+
+
+
