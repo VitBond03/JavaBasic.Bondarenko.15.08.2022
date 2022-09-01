b4280e2cb8be02d9282e5d44facf5d9d7cee09af
VitBond03 <111899487+VitBond03@users.noreply.github.com>
2022-09-01 18:01:04 +0300

    Update HomeWork5.java

--- src/Main/Java/com/hillel/bondarenko/lessons/homework5/HomeWork5.java
+++ src/Main/Java/com/hillel/bondarenko/lessons/homework5/HomeWork5.java
@@ -4,19 +4,10 @@ import java.util.Scanner;
 
 public class HomeWork5 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Please enter team1 name");
-        String str = scanner.nextLine();
-        System.out.println("You entered" + str);
-        int intVariable = -1;
-        System.out.println("Please enter frags for player1");
-        if (scanner.hasNextInt()) {
-            intVariable = scanner.nextInt();
-            System.out.println("You entered number: " + intVariable);
-        } else {
-            System.out.println("WRONG DATA,RESTART!");
+      
+      
 
-        }
+        
     }
 }
 
